9d6259cae32efb0c322a6a87ed29202ed2519775
Matias Insaurralde <matias@insaurral.de>
2015-06-25 02:29:17 -0400

    async auth and ready callback

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const Browser = require( 'zombie' );
+const Browser = require( 'zombie' ),
+      browser = new Browser();
 
-const browser = new Browser();
+function soundslice( email, password ) {
+  console.log( 'soundslice: init/auth' );
 
-browser.visit( 'https://www.soundslice.com/login/', function() {
-  console.log( 'get login page', browser.text( 'title' ) );
-  browser.fill( 'email', 'matias@insaurral.de' )
-         .fill( 'password', 'prueba12' )
-         .pressButton( 'Log in', function() {
-           console.log( 'presionar boton!', browser.text( 'title' ) );
-         })
-  console.log( 'post fill', browser.text( 'title' ) );
-} );
+  var self = this;
+
+  this.authenticated = false;
+  this.readyEvent = function() {};
+
+  browser.visit( 'https://www.soundslice.com/login/', function() {
+    browser.fill( 'email', email )
+           .fill( 'password', password )
+           .pressButton( 'Log in', function() {
+             if( browser.text( 'title' ).indexOf( 'Log in' ) == -1 ) {
+               console.log( 'soundslice: auth ok' );
+               self.authenticated = true;
+               self.readyEvent();
+             } else {
+               console.log( 'soundslice: auth error' );
+             };
+           })
+  });
+};
+
+soundslice.prototype.uploadNotation = function() {
+  console.log( 'soundslice: uploadNotation' );
+};
+
+soundslice.prototype.ready = function( f ) {
+  this.readyEvent = f;
+};
+
+module.exports = soundslice;
